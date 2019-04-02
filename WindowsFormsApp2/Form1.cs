5eb92e6e39d1c3bdc8f4fc738bfcd4364614adff
= <ivan.programmer@bk.ru>
2019-04-02 14:48:59 +0400

    First commit

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double firstnum;
+        bool plus = false;
+        bool minus = false;
+        bool ymnog = false;
+        bool delenie = false;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += (sender as Button).Text;
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                double x;
+                x = Convert.ToDouble(textBox1.Text) * Convert.ToDouble(textBox1.Text);
+                textBox1.Text = Convert.ToString(x);
+            }
+            else
+            {
+                return;
+            }
+
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                double k;
+                k = Convert.ToDouble(textBox1.Text);
+                textBox1.Text = Convert.ToString(Math.Sqrt(k));
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            plus = true;
+            textBox1.Tag = textBox1.Text;
+            textBox1.Text = "";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if (plus)
+            {
+            
+            }
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            minus = true;
+            textBox1.Tag = textBox1.Text;
+            textBox1.Text = "";
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            delenie = true;
+            textBox1.Tag = textBox1.Text;
+            textBox1.Text = "";
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            ymnog = true;
+            textBox1.Tag = textBox1.Text;
+            textBox1.Text = "";
+        }
+    }
+}
